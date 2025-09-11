6713acc14c52c36fa92e9242e2778c0458e220c9
Felipe Genistretti Rodrigues <genistrettifelipe@gmail.com>
2025-09-11 10:58:10 -0300

    Implementando rota de placa GGI4005

--- app/Http/Controllers/ConsultaController.php
+++ app/Http/Controllers/ConsultaController.php
@@ -10,11 +10,12 @@ class ConsultaController extends Controller
     public function ConsultarPlaca(Request $request){
 
         try{
-            
+            $path = base_path("Consultas/GGI4005.json"); 
+            $fileData = json_decode(file_get_contents($path), true);
             $vericar = $request->validate([
-                'placa'=>'required|string|max:7'
+                'placa'=>'required|string|max:7|in:GGI4005,ggi4005'
             ]);
-            return response()->json(['sucesso'=>'Placa consultada com sucesso!'], 200);
+            return response()->json(['sucesso'=>'Placa consultada com sucesso!', 'teste'=>$fileData], 200);
 
         }catch(Exception $e){
             return response()->json(['erro'=>'Erro ao consultar a placa:', $e->getMessage()], 500);
