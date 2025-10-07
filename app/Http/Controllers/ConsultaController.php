103c7467fc9edab8c9cc4061f9cd171a6fae5a71
Felipe Genistretti Rodrigues <genistrettifelipe@gmail.com>
2025-10-07 12:02:38 -0300

    alterando as mensagens de retorno da api

--- app/Http/Controllers/ConsultaController.php
+++ app/Http/Controllers/ConsultaController.php
@@ -19,7 +19,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/Condutores/consulta-03.json",
-            "Consulta realizada com sucesso"
         );
     }
 
@@ -28,7 +27,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/Infracoes/infracoes-5.json",
-            "Consulta realizada com sucesso"
         );
     }
 
@@ -37,7 +35,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/Infracoes/infracoes-19.json",
-            "Consulta realizada com sucesso"
         );
     }
 
@@ -46,7 +43,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/DadosCadastrais/dados-cadastrais-cpf-6.json",
-            "Consulta realizada com sucesso"
         );
     }
 
@@ -55,7 +51,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/DadosCadastrais/dados-cadastrais-cpf-7.json",
-            "Consulta realizada com sucesso"
         );
     }
 
@@ -64,7 +59,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/DadosCadastrais/dados-cadastrais-cpf-8.json",
-            "Consulta realizada com sucesso"
         );
     }
 
@@ -73,7 +67,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/DadosCadastrais/dados-cadastrais-cnpj-9.json",
-            "Consulta realizada com sucesso"
         );
     }
 
@@ -82,7 +75,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/DadosCadastrais/dados-cadastrais-cnpj-10.json",
-            "Consulta realizada com sucesso"
         );
     }
 
@@ -91,7 +83,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/Veiculos/veiculos-01.json",
-            "Consulta 01 PRECIFICAÇÃO FIPE POR PLACA REALIZADA COM SUCESSO"
         );
     }
 
@@ -100,7 +91,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/Veiculos/veiculos-02.json",
-            "Consulta 02 VEÍCULOS COM PASSAGEM EM LEILÃO POR PLACA REALIZADA COM SUCESSO"
         );
     }
 
@@ -109,7 +99,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/Veiculos/veiculos-04.json",
-            "Consulta 04 DADOS COMPLETOS DO VEICULO E RESTRIÇÃO POR PLACA REALIZADA COM SUCESSO"
         );
     }
 
@@ -118,7 +107,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/Veiculos/veiculos-12.json",
-            "Consulta 12 PREÇO TABELA FIPE POR PLACA REALIZADA COM SUCESSO"
         );
     }
 
@@ -127,7 +115,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/Veiculos/veiculos-13.json",
-            "Consulta 13 VEICULO COM PASSAGEM EM LEILÃO POR PLACA REALIZADA COM SUCESSO"
         );
     }
 
@@ -136,7 +123,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/Veiculos/veiculos-14.json",
-            "Consulta 14 DÉBITOS E MULTAS POR PLACA REALIZADA COM SUCESSO"
         );
     }
 
@@ -145,7 +131,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/Veiculos/veiculos-16.json",
-            "Consulta 16 DADOS COMPLETOS DO VEICULO E RESTRIÇÕES POR CHASSI REALIZADA COM SUCESSO"
         );
     }
 
@@ -154,7 +139,6 @@ class ConsultaController extends Controller
         $request->validated();
         return $this->respondWithJson(
             "Consultas/Veiculos/veiculos-17.json",
-            "Consulta 17 DADOS COMPLETOS DO VEICULO E RESTRIÇÕES POR RENAVAM REALIZADA COM SUCESSO"
         );
     }
 
@@ -166,7 +150,6 @@ class ConsultaController extends Controller
 
         return $this->respondWithJson(
             "Consultas/Veiculos/veiculos-18.json",
-            "Consulta 18 DADOS COMPLETOS DO VEICULO E RESTRIÇÕES POR CPF OU CNPJ REALIZADA COM SUCESSO"
         );
     }
 }
